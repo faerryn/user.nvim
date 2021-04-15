acc90fc05a9ca9cd64c6a71df11b8c1fb18cebbe
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-15 14:18:16 -0400

    First test

---
+++ tests/init.lua
@@ -0,0 +1,24 @@
+-- nvim -u tests/init.lua
+-- :messages
+
+vim.o.rtp = ".,"..vim.o.rtp
+
+local user = require'user'
+user.setup()
+local use = user.use
+
+use {
+	"gruvbox-community/gruvbox",
+	after = "norcalli/nvim.lua",
+	config = function()
+		vim.api.nvim_command("colorscheme gruvbox")
+		print("gruvbox")
+	end,
+}
+
+use {
+	"norcalli/nvim.lua",
+	config = function()
+		print("nvim.lua")
+	end,
+}
