fb11cda9d4137fd76ee57b2d58f983a8b2c3c635
Faerryn <alexandre.liao@gmail.com>
2022-05-10 00:31:18 -0500

    format using stylua; introduce a functional clean()

--- lua/user.lua
+++ lua/user.lua
@@ -6,62 +6,62 @@ local packman
 -- requesting an runtimepath edit.
 --]]
 local function use(args)
-  local pack = {}
+    local pack = {}
 
-  if type(args) == "string" then
-    pack.name = args
-  elseif type(args) == "table" then
-    if args.disabled then
-      return
+    if type(args) == "string" then
+        pack.name = args
+    elseif type(args) == "table" then
+        if args.disabled then
+            return
+        end
+
+        pack.name = args[1]
+
+        pack.repo = args.repo
+        pack.branch = args.branch
+        pack.pin = args.pin
+
+        pack.subdir = args.subdir
+
+        pack.init = args.init
+        pack.config = args.config
+
+        pack.install = args.install
+        pack.update = args.update
+
+        pack.after = args.after
+    else
+        error "user.use -- invalid args"
     end
 
-    pack.name = args[1]
+    if packman.packs[pack.name] then
+        return packman.packs[pack.name]
+    end
 
-    pack.repo = args.repo
-    pack.branch = args.branch
-    pack.pin = args.pin
+    -- we have a repo that can be managed by packman
+    if pack.repo or string.match(pack.name, "^[^/]+/[^/]+$") then
+        pack.repo = pack.repo or "https://github.com/" .. pack.name .. ".git"
+        return packman:request(pack)
+    end
 
-    pack.subdir = args.subdir
-
-    pack.init = args.init
-    pack.config = args.config
-
-    pack.install = args.install
-    pack.update = args.update
-
-    pack.after = args.after
-  else
-    error("user.use -- invalid args")
-  end
-
-  if packman.packs[pack.name] then
-    return packman.packs[pack.name]
-  end
-
-  -- we have a repo that can be managed by packman
-  if pack.repo or string.match(pack.name, "^[^/]+/[^/]+$") then
-    pack.repo = pack.repo or "https://github.com/"..pack.name..".git"
-    return packman:request(pack)
-  end
-
-  -- we can install local directories too!
-  local path = vim.fn.fnamemodify(pack.name, ":p")
-  if vim.fn.isdirectory(path) then
-    vim.opt.runtimepath:prepend(path)
-  else
-    error("user.user -- invalid args")
-  end
-  return pack
+    -- we can install local directories too!
+    local path = vim.fn.fnamemodify(pack.name, ":p")
+    if vim.fn.isdirectory(path) then
+        vim.opt.runtimepath:prepend(path)
+    else
+        error "user.user -- invalid args"
+    end
+    return pack
 end
 
 --[[
 -- instantiate everything
 --]]
 local function setup(args)
-  if args and args.path then
-    args.path = vim.fn.expand(args.path)
-  end
-  packman = require("user.packman").PackMan:new(args)
+    if args and args.path then
+        args.path = vim.fn.expand(args.path)
+    end
+    packman = require("user.packman").PackMan:new(args)
 end
 
 --[[
@@ -69,28 +69,31 @@ end
 -- does nothing if parallel jobs are not enabled
 --]]
 local function flush()
-  if packman.parallel then
-    packman:flush_jobs()
-    packman:flush_packadd_queue()
-  end
+    if packman.parallel then
+        packman:flush_jobs()
+        packman:flush_packadd_queue()
+    end
 end
 
 --[[
 -- updates packages
--- awaits parallel git pull jobs if enabled
+-- don't forget to flush if parallel is enabled
 --]]
 local function update()
-  packman:update_all()
-  if packman.parallel then
-    packman:flush_jobs()
-  end
+    packman:update_all()
+end
+
+local function clean()
+    packman:clean()
 end
 
 return {
-  setup = setup,
-  update = update,
-  use = use,
+    setup = setup,
+    use = use,
+    flush = flush,
 
-  flush = flush,
-  startup = flush,
+    update = update,
+    clean = clean,
+
+    startup = flush,
 }
--- lua/user/packman.lua
+++ lua/user/packman.lua
@@ -1,176 +1,197 @@
 local Deque = require("user.deque").Deque
 
 local function gen_helptags(pack)
-  vim.api.nvim_command("silent! helptags "..vim.fn.fnameescape(pack.install_path).."/doc")
+    vim.api.nvim_command("silent! helptags " .. vim.fn.fnameescape(pack.install_path) .. "/doc")
 end
 
 local function git_head_hash(pack)
-  return vim.fn.system([[git -C "]]..pack.install_path..[[" rev-parse HEAD]])
+    return vim.fn.system([[git -C "]] .. pack.install_path .. [[" rev-parse HEAD]])
 end
 
 local function packadd(pack)
-  vim.api.nvim_command("packadd "..vim.fn.fnameescape(pack.packadd_path))
-  -- work around vim#1994
-  if vim.v.vim_did_enter == 1 then
-    for after_source in vim.fn.glob(pack.install_path.."/after/plugin/**/*.vim"):gmatch("[^\n]+") do
-      vim.api.nvim_command("source "..vim.fn.fnameescape(after_source))
+    vim.api.nvim_command("packadd " .. vim.fn.fnameescape(pack.packadd_path))
+    -- work around vim#1994
+    if vim.v.vim_did_enter == 1 then
+        for after_source in vim.fn.glob(pack.install_path .. "/after/plugin/**/*.vim"):gmatch "[^\n]+" do
+            vim.api.nvim_command("source " .. vim.fn.fnameescape(after_source))
+        end
     end
-  end
 end
 
 local function chdir_do_fun(dir, fun)
-  local cwd = vim.loop.cwd()
-  vim.loop.chdir(dir)
-  pcall(fun)
-  vim.loop.chdir(cwd)
+    local cwd = vim.loop.cwd()
+    vim.loop.chdir(dir)
+    pcall(fun)
+    vim.loop.chdir(cwd)
 end
 
 local function post_install(pack)
-  if pack.pin then
-    local escaped_install_path = vim.fn.shellescape(pack.install_path)
-    os.execute("git -C "..escaped_install_path.." checkout --quiet "..pack.pin)
-  end
-  gen_helptags(pack)
-  if pack.install then
-    chdir_do_fun(pack.install_path, pack.install)
-  end
+    if pack.pin then
+        local escaped_install_path = vim.fn.shellescape(pack.install_path)
+        os.execute("git -C " .. escaped_install_path .. " checkout --quiet " .. pack.pin)
+    end
+    gen_helptags(pack)
+    if pack.install then
+        chdir_do_fun(pack.install_path, pack.install)
+    end
 end
 
 local function post_update(pack)
-  local hash = git_head_hash(pack)
-  if pack.hash and pack.hash ~= hash then
-    gen_helptags(pack)
-    if pack.update then
-      chdir_do_fun(pack.install_path, pack.update)
+    local hash = git_head_hash(pack)
+    if pack.hash and pack.hash ~= hash then
+        gen_helptags(pack)
+        if pack.update then
+            chdir_do_fun(pack.install_path, pack.update)
+        end
+        pack.hash = hash
     end
-    pack.hash = hash
-  end
 end
 
 local PackMan = {}
 
 function PackMan:new(args)
-  args = args or {}
-  local packman = {
-    path = (args.path and vim.fn.resolve(vim.fn.fnamemodify(args.path, ":p"))) or vim.fn.stdpath("data").."/site/pack/user/",
+    args = args or {}
+    local packman = {
+        path = (args.path and vim.fn.resolve(vim.fn.fnamemodify(args.path, ":p")))
+            or vim.fn.stdpath "data" .. "/site/pack/user/",
 
-    packs = {},
+        packs = {},
 
-    packadd_queue = Deque:new(),
+        packadd_queue = Deque:new(),
 
-    parallel = args.parallel or false,
-  }
-  self.__index = self
-  setmetatable(packman, self)
-  return packman
+        parallel = args.parallel or false,
+    }
+    self.__index = self
+    setmetatable(packman, self)
+    return packman
 end
 
 function PackMan:install(pack)
-  if vim.fn.isdirectory(pack.install_path) == 1 then
-    return
-  end
+    if vim.fn.isdirectory(pack.install_path) == 1 then
+        return
+    end
 
-  local command = "git clone --quiet --recurse-submodules --shallow-submodules "
+    local command = "git clone --quiet --recurse-submodules --shallow-submodules "
 
-  if not pack.pin then
-    command = command.."--depth 1 "
-  end
+    if not pack.pin then
+        command = command .. "--depth 1 "
+    end
 
-  if pack.branch then
-    command = command.."--branch "..vim.fn.shellescape(pack.branch).." "
-  end
+    if pack.branch then
+        command = command .. "--branch " .. vim.fn.shellescape(pack.branch) .. " "
+    end
 
-  local escaped_install_path = vim.fn.shellescape(pack.install_path)
-  command = command..vim.fn.shellescape(pack.repo).." "..escaped_install_path
+    local escaped_install_path = vim.fn.shellescape(pack.install_path)
+    command = command .. vim.fn.shellescape(pack.repo) .. " " .. escaped_install_path
 
-  if self.parallel then
-    pack.install_job = io.popen(command, "r")
-  else
-    os.execute(command)
-    post_install(pack)
-  end
+    if self.parallel then
+        pack.install_job = io.popen(command, "r")
+    else
+        os.execute(command)
+        post_install(pack)
+    end
 end
 
 function PackMan:update(pack)
-  if not pack.pin then
-    pack.hash = git_head_hash(pack)
+    if not pack.pin then
+        pack.hash = git_head_hash(pack)
 
-    local escaped_install_path = vim.fn.shellescape(pack.install_path)
-    local command = "git -C "..escaped_install_path.." pull --quiet --recurse-submodules --update-shallow"
+        local escaped_install_path = vim.fn.shellescape(pack.install_path)
+        local command = "git -C " .. escaped_install_path .. " pull --quiet --recurse-submodules --update-shallow"
 
-    if self.parallel then
-      pack.update_job = io.popen(command, "r")
-    else
-      os.execute(command)
-      post_update(pack)
+        if self.parallel then
+            pack.update_job = io.popen(command, "r")
+        else
+            os.execute(command)
+            post_update(pack)
+        end
     end
-  end
 end
 
 function PackMan:request(pack)
-  if self.packs[pack.name] then
-    return self.packs[pack.name]
-  end
-  self.packs[pack.name] = pack
+    if self.packs[pack.name] then
+        return self.packs[pack.name]
+    end
+    self.packs[pack.name] = pack
 
-  if pack.init then pack.init() end
+    if pack.init then
+        pack.init()
+    end
 
-  local install_path = pack.name
-  if pack.branch then
-    install_path = install_path.."/branch/"..pack.branch
-  else
-    install_path = install_path.."/default/default"
-  end
-  if pack.pin then
-    install_path = install_path.."/commit/"..pack.pin
-  else
-    install_path = install_path.."/default/default"
-  end
+    local install_path = pack.name:gsub("_", "__"):gsub("/", "_")
+    if pack.branch then
+        install_path = install_path .. "/branch/" .. pack.branch
+    else
+        install_path = install_path .. "/default/default"
+    end
+    if pack.pin then
+        install_path = install_path .. "/commit/" .. pack.pin
+    else
+        install_path = install_path .. "/default/default"
+    end
 
-  local packadd_path = install_path
-  if pack.subdir then packadd_path = packadd_path.."/"..pack.subdir end
+    local packadd_path = install_path
+    if pack.subdir then
+        packadd_path = packadd_path .. "/" .. pack.subdir
+    end
 
-  pack.packadd_path = packadd_path
-  pack.install_path = self.path.."/opt/"..install_path
+    pack.packadd_path = packadd_path
+    pack.install_path = self.path .. "/opt/" .. install_path
 
-  self:install(pack)
-  if self.parallel then
-    self.packadd_queue:push_back(pack)
-  else
-    packadd(pack)
-    if pack.config then pack.config() end
-  end
+    self:install(pack)
+    if self.parallel then
+        self.packadd_queue:push_back(pack)
+    else
+        packadd(pack)
+        if pack.config then
+            pack.config()
+        end
+    end
 
-  return pack
+    return pack
 end
 
 function PackMan:flush_jobs()
-  for _, pack in pairs(self.packs) do
-    if pack.install_job then
-      pack.install_job:close()
-      pack.install_job = nil
-      post_install(pack)
+    for _, pack in pairs(self.packs) do
+        if pack.install_job then
+            pack.install_job:close()
+            pack.install_job = nil
+            post_install(pack)
+        end
+        if pack.update_job then
+            pack.update_job:close()
+            pack.update_job = nil
+            post_update(pack)
+        end
     end
-    if pack.update_job then
-      pack.update_job:close()
-      pack.update_job = nil
-      post_update(pack)
-    end
-  end
 end
 
 function PackMan:flush_packadd_queue()
-  while self.packadd_queue:len() > 0 do
-    local pack = self.packadd_queue:pop_front()
-    packadd(pack)
-    if pack.config then pack.config() end
-  end
+    while self.packadd_queue:len() > 0 do
+        local pack = self.packadd_queue:pop_front()
+        packadd(pack)
+        if pack.config then
+            pack.config()
+        end
+    end
 end
 
 function PackMan:update_all()
-  for _, pack in pairs(self.packs) do
-    self:update(pack)
-  end
+    for _, pack in pairs(self.packs) do
+        self:update(pack)
+    end
+end
+
+function PackMan:clean()
+    local keep = {}
+    for _, pack in pairs(self.packs) do
+        keep[vim.fn.resolve(pack.install_path)] = true
+    end
+    for place in vim.fn.expand(vim.fn.stdpath "data" .. "/site/pack/user/opt/*/*/*/*/*"):gmatch "[^\n]+" do
+        if not keep[place] then
+            vim.fn.delete(place, "rf")
+        end
+    end
 end
 
 return { PackMan = PackMan }
